ab9485ed7e07be6b9c73f5ebe380fd07f51bb9b6
mallardeau yoan <mallardeauyoan@mallardeaus-MacBook-Air.local>
2026-03-17 16:45:49 +0100

    pseudo code pour image->vecteur

---
+++ preprocessing/build_dataset.py
@@ -0,0 +1,28 @@
+##DEBUT
+
+#    definir le chemin du dataset
+#    definir la liste des classes :
+#        - hello_kitty
+#        - fake_hello_kitty
+#        - other
+
+#    creer une liste X pour stocker les vecteurs d’images
+#    creer une liste y pour stocker les labels
+
+#    pour chaque classe dans la liste des classes :
+#        recuperer le dossier correspondant
+
+#        pour chaque image dans ce dossier :
+#            lire l’image
+#            verifier qu’elle est valide
+
+#    convertir X en matrice
+#    convertir y en vecteur
+
+#    sauvegarder X et y
+#    afficher :
+#        - nombre total d’images
+#        - taille d’un vecteur
+#        - nombre d’images par classe
+
+#FIN
